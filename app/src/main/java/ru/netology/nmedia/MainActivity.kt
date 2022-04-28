5c13706e5c9c055c8a033a8a357d1da505a84c12
Aleksey Anokhin <anoxin-aleksey@bk.ru>
2022-04-28 11:03:52 +0300

    Binding added

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -2,10 +2,17 @@ package ru.netology.nmedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.netology.nmedia.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.post_list_item)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.likesAmountImage.setOnClickListener {
+            binding.likesAmountImage.setImageResource(R.drawable.ic_liked_24)
+        }
     }
 }
