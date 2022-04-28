4c9603ed04b74901869ef3342cefaa740ccca91e
Aleksey Anokhin <anoxin-aleksey@bk.ru>
2022-04-28 14:33:04 +0300

    Liked fun added

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -2,17 +2,44 @@ package ru.netology.nmedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.DrawableRes
 import ru.netology.nmedia.databinding.ActivityMainBinding
+import ru.netology.nmedia.dto.Post
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val post = Post(
+            id = 1,
+            author = "Нетология. Университет интернет-профессий будущего",
+            content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
+            published = "21 мая в 18:36",
+            likedByMe = false
+        )
+
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.render(post)
         binding.likesAmountImage.setOnClickListener {
-            binding.likesAmountImage.setImageResource(R.drawable.ic_liked_24)
+            post.likedByMe = !post.likedByMe
+            binding.likesAmountImage.setImageResource(getLikeIconResId(post.likedByMe))
         }
+
     }
+
+    private fun ActivityMainBinding.render(post: Post) {
+        authorName.text = post.author
+        postContent.text = post.content
+        published.text = post.published
+        likesAmountImage.setImageResource(getLikeIconResId(post.likedByMe))
+    }
+
+    @DrawableRes
+    private fun getLikeIconResId(liked: Boolean) =
+        if (liked) R.drawable.ic_liked_24 else R.drawable.ic_like_border_24
+
+
 }
