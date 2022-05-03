efe555a6763e8bff544f0232505e087bb7c3002d
Aleksey Anokhin <anoxin-aleksey@bk.ru>
2022-05-03 14:38:46 +0300

    MVVM

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -2,7 +2,9 @@ package ru.netology.nmedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.annotation.DrawableRes
+import ru.netology.nmedia.ViewModel.PostViewModel
 import ru.netology.nmedia.databinding.ActivityMainBinding
 import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.utils.Utils
@@ -10,35 +12,25 @@ import kotlin.math.floor
 
 
 class MainActivity : AppCompatActivity() {
+
+    val viewModel by viewModels<PostViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val post = Post(
-            id = 1,
-            author = "Нетология. Университет интернет-профессий будущего",
-            content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
-            published = "21 мая в 18:36",
-            likes = 5099,
-            likedByMe = false,
-            shared = 9999,
-            viewed = 1_999_999
-        )
+        viewModel.data.observe(this) { post ->
+            binding.render(post)
+        }
 
-        binding.render(post)
         binding.likesAmountImage.setOnClickListener {
-            post.likedByMe = !post.likedByMe
-            binding.likesAmountImage.setImageResource(getLikeIconResId(post.likedByMe))
-            if (post.likedByMe) post.likes++ else post.likes--
-            binding.likesAmount.text = Utils.formatActivitiesOnPost(post.likes)
+            viewModel.onLikedClicked()
         }
 
         binding.sharedImage.setOnClickListener {
-            post.shared++
-            binding.sharedAmount.text = Utils.formatActivitiesOnPost(post.shared)
+            viewModel.onShareClicked()
         }
-
     }
 
     private fun ActivityMainBinding.render(post: Post) {
@@ -54,5 +46,4 @@ class MainActivity : AppCompatActivity() {
     @DrawableRes
     private fun getLikeIconResId(liked: Boolean) =
         if (liked) R.drawable.ic_liked_24 else R.drawable.ic_like_border_24
-
 }
---
+++ app/src/main/java/ru/netology/nmedia/data/impl/InMemoryPostRepository.kt
@@ -0,0 +1,42 @@
+package ru.netology.nmedia.data.impl
+
+import ru.netology.nmedia.data.PostRepository
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import ru.netology.nmedia.dto.Post
+
+class InMemoryPostRepository : PostRepository {
+    override val data = MutableLiveData(
+        Post(
+            id = 1,
+            author = "Нетология. Университет интернет-профессий будущего",
+            content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
+            published = "21 мая в 18:36",
+            likes = 5099,
+            likedByMe = false,
+            shared = 9999,
+            viewed = 1_999_999
+        )
+
+    )
+
+    override fun like() {
+        val currentPost = checkNotNull(data.value) {
+            "Data value should not be null"
+        }
+        val likedPost = currentPost.copy(
+            likedByMe = !currentPost.likedByMe
+        )
+        data.value = likedPost
+    }
+
+    override fun share() {
+        val currentPost = checkNotNull(data.value) {
+            "Data value should not be null"
+        }
+        val sharedPost = currentPost.copy(
+            shared = currentPost.shared + 1
+        )
+        data.value = sharedPost
+    }
+}
